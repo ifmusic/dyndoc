bc0a3a944e0b4a22e9586808ff574f9d5f7cf6d2
Rodrigo Jimenez <ifmusic@gmail.com>
2017-04-01 23:26:00 -0300

    PoC

--- py_netstat.py
+++ py_netstat.py
@@ -1,4 +1,14 @@
 from subprocess import Popen, PIPE
-p1 = Popen(['lsof', '-a', '-i4'], stdout=PIPE)
-p2 = Popen(["grep", "LISTEN"], stdin=p1.stdout, stdout=PIPE)
-output = p2.communicate()[0]
+import re
+#p0 = Popen(['lsof', '-a', '-i4'])
+p1 = Popen(['lsof',  '-i4'], stdout=PIPE)
+while True:
+  line = p1.stdout.readline()
+  if line != '':
+    #the real code does filtering here
+    if line.find("LISTEN") > 0:
+    	print  line.rstrip()
+    	matchObj = re.findall( r"(?P<name>\w+)\s+(?P<pid>\d+)(?:.+)TCP\s+(?P<host>.+):(?P<port>\d+)(?: )\(LISTEN\)", line)
+    	print matchObj
+  else:
+    break
