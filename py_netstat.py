9b4f4a8bbe750c181a39cc933e0039224eea3e95
Rodrigo Jimenez <ifmusic@gmail.com>
2017-04-08 20:40:01 -0300

    ejemplo de db y cambios en netstat

--- py_netstat.py
+++ py_netstat.py
@@ -1,14 +1,36 @@
 from subprocess import Popen, PIPE
-import re
-#p0 = Popen(['lsof', '-a', '-i4'])
-p1 = Popen(['lsof',  '-i4', '-P'], stdout=PIPE)
+import re,  pprint, pickle
+import socket
+
+DEBUG = 1
+
+def Print( s ):
+	if DEBUG : print s
+
+listening_apps = []
+
+fqdn = socket.gethostbyaddr(socket.gethostname())[0]
+#TODO : handle errors calling lsof
+msg = "Obteniendo servicios en "+ (fqdn) + "..."
+Print(msg)
+
+p1 = Popen(['lsof', '-P'], stdout=PIPE)
 while True:
   line = p1.stdout.readline()
   if line != '':
     #the real code does filtering here
-    if line.find("LISTEN") > 0:
-    	print  line.rstrip()
     	matchObj = re.findall( r"(?P<name>\w+)\s+(?P<pid>\d+)(?:.+)TCP\s+(?P<host>.+):(?P<port>\d+)(?: )\(LISTEN\)", line)
-    	print matchObj
+    	if matchObj != [] : 
+    	 Print(matchObj)
+    	 listening_apps.append(matchObj)
   else:
-    break
+    break 
+#TO DO: Handle write errors
+output = open('data.pkl', 'wb')
+
+# Pickle the list using the highest protocol available.
+pickle.dump(listening_apps, output, -1)
+output.close()
+
+
+
