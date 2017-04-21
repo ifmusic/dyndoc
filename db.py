d5b2e26658d53734159f01c8d3b213175ef25610
Rodrigo Jimenez <ifmusic@gmail.com>
2017-04-20 23:22:15 -0300

    A medio laburo... Avances en el playbook de ansible, Falta pasar la data del binario en pickle hacia la DB

--- db.py
+++ db.py
@@ -1,13 +1,28 @@
 import sqlite3
+import pprint, pickle
+
+#TODO: put this pickle code aside...
+
+pkl_file = open('data-192.168.0.17', 'rb')
+
+data1 = pickle.load(pkl_file)
+pprint.pprint(data1)
+
+
+pkl_file.close()
+
+
+
 conn = sqlite3.connect('db_dyndoc.db')
 
 conn.execute('DELETE FROM hosts')
 
 conn.commit()
 
-#En el ejemplo, paso la PK como None o Null para que sqlite autoasigne un uint64++
+# PK as Null gets autoassinged as a uint64++
 hosts = [(None,'10.0.0.1', '', 'host01.local'),
-          (None, '10.0.0.2', '', 'host02.local')
+          (None, '10.0.0.2', '', 'host02.local'),
+           (None, '10.0.0.3', '', 'host03.local')
             ]
 conn.executemany('INSERT INTO hosts VALUES (?,?,?,?)', hosts)
 
