d3993438c6ed476faaaa26c0ce966aa216461005
Rodrigo Jimenez <ifmusic@gmail.com>
2017-04-23 23:34:30 -0300

    agrego tratado de errores, variablizacionn

--- db.py
+++ db.py
@@ -1,30 +1,45 @@
+
 import sqlite3
 import pprint, pickle
+import sys
 
 #TODO: put this pickle code aside...
 
-pkl_file = open('data-192.168.0.17', 'rb')
+#Missing C style forward function declarations
+def get_hostid_byIPv4(ipv4):
+	try:
+		conn = sqlite3.connect('db_dyndoc.db')
+		c.execute('SELECT * FROM hosts WHERE ipv4=?', ipv4)
+		return c.fetchone()
+	except:
+		print "Unexpected error:", sys.exc_info()[0]
+		raise
 
+if len(sys.argv) == 1:
+	print 'Must specify a file to unpickle...'
+	exit()
+else:
+	file_to_unpickle =  sys.argv[1]
+
+#unpack binary file with hosts services data
+pkl_file = open(file_to_unpickle, 'rb') # Described as insecure; people need to take some chances
 data1 = pickle.load(pkl_file)
 pprint.pprint(data1)
-
-
 pkl_file.close()
 
-
-
+#Load that data into the DB
 conn = sqlite3.connect('db_dyndoc.db')
+#conn.execute('DELETE FROM hosts')
+#conn.commit()
 
-conn.execute('DELETE FROM hosts')
+#DB> binding, tcpportbinding , processname, pid
+#PY> ProcessName, Pid, binding, tcpport
 
-conn.commit()
+#We have to change the natural order of columns and values
+# id_service_id , id_host , ip_binding,tcpport_binding,processname,pid 
 
-# PK as Null gets autoassinged as a uint64++
-hosts = [(None,'10.0.0.1', '', 'host01.local'),
-          (None, '10.0.0.2', '', 'host02.local'),
-           (None, '10.0.0.3', '', 'host03.local')
-            ]
-conn.executemany('INSERT INTO hosts VALUES (?,?,?,?)', hosts)
+conn.executemany('INSERT INTO hosts_services  VALUES (?,?,?,?,?)', data1)
+
 
 # Save (commit) the changes
 conn.commit()
--- py_netstat.py
+++ py_netstat.py
@@ -1,11 +1,18 @@
 from subprocess import Popen, PIPE
 import re,  pprint, pickle
 import socket
+import sys
 
 DEBUG = 1
 
 def Print( s ):
 	if DEBUG : print s
+
+if len(sys.argv) == 1:
+  print 'Must specify a file to pickle...'
+  exit()
+else:
+  file_to_pickle =  sys.argv[1]
 
 listening_apps = []
 
@@ -14,19 +21,26 @@ fqdn = socket.gethostbyaddr(socket.gethostname())[0]
 msg = "Obteniendo servicios en "+ (fqdn) + "..."
 Print(msg)
 
-p1 = Popen(['lsof', '-P'], stdout=PIPE)
+p1 = Popen(['lsof', '-P','-n', '-i'], stdout=PIPE)
 while True:
   line = p1.stdout.readline()
   if line != '':
-    #the real code does filtering here
-    	matchObj = re.findall( r"(?P<name>\w+)\s+(?P<pid>\d+)(?:.+)TCP\s+(?P<host>.+):(?P<port>\d+)(?: )\(LISTEN\)", line)
-    	if matchObj != [] : 
-    	 Print(matchObj)
-    	 listening_apps.append(matchObj)
+      #the real code does filtering here
+      matchObj = re.findall( r"(?P<name>\w+)\s+(?P<pid>\d+)(?:.+)TCP\s+(?P<host>.+):(?P<port>\d+)(?: )\(LISTEN\)", line)
+      #Obtiene:
+      #ProcessName, Pid, binding, tcpport
+      if matchObj != []:
+       tmp=[]
+       host_id = fqdn
+       tmp.append(host_id)
+       matchObj=list(matchObj[0])
+       tmp.extend(matchObj)
+       listening_apps.append(tmp)
+       Print(tmp)
   else:
     break 
 #TO DO: Handle write errors
-output = open('/tmp/data.pkl', 'wb')
+output = open('/tmp/services['+file_to_pickle+'].pkl', 'wb')
 
 # Pickle the list using the highest protocol available.
 pickle.dump(listening_apps, output, -1)
